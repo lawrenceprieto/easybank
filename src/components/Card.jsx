4e52ab0716ce196ffbebddf15e5980246688379e
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-21 12:19:11 +0800

    add card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,35 @@
+import Card from 'react-bootstrap/Card';
+
+function CardComponent(props) {
+    return (
+        <>
+            <Card style={{ width: "100%", border: "none"}}>
+                <Card.Img variant="top" src={props.image} alt={props.title} />
+                <Card.Body style={{padding: "20px 30px 40px 30px"}}>
+                    <Card.Text style={{
+                            color: "hsl(233, 8%, 62%)", 
+                            margin: "0"
+                        }}>
+                        By {props.author}
+                    </Card.Text>
+                    <Card.Title style={{
+                            maxHeight: "50px", 
+                            overflow: "hidden",
+                            margin: "10px 0"
+                        }}>
+                        {props.title}
+                    </Card.Title>
+                    <Card.Text style={{
+                            color: "hsl(233, 8%, 62%)",
+                            maxHeight: "100px",
+                            overflow: "hidden"
+                        }}>
+                        {props.about}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </>
+    )
+}
+
+export default CardComponent
